d9a375dbddd5d1d57b2766cfb30bfadf9ba73c1c
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-04-02 03:26:01 +0530

    used map method for options

--- src/components/Random-Color/ColorSelector.jsx
+++ src/components/Random-Color/ColorSelector.jsx
@@ -4,7 +4,7 @@ const ColorSelector = () => {
   const Hexcode = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 
   // manage state for selected color type
-  const [selectedValue, setSelectedValue] = useState("hex");
+  const [selectedValue, setSelectedValue] = useState("HEX");
 
   // manage state for current color
   const [currentColor, setCurrentColor] = useState("#FFFFF");
@@ -48,25 +48,31 @@ const ColorSelector = () => {
   // Handle the generate button eventand trigger respective type color
   const HandleRandomGenerate = () => {
     switch (selectedValue) {
-      case "hex": {
+      case "HEX": {
       } RandomHexColor();
         break;
-      case "rgb": {
+      case "RGB": {
       } RandomRGB();
         break;
-      case "hsl": {
+      case "HSL": {
       } RandomHSL();
         break;
     }
   }
-
+  const colorType = ["HEX", "RGB", "HSL"];
   return (
     <div className='flex h-screen flex-col items-center gap-28' style={{ backgroundColor: currentColor }}>
       <div className="nav w-full bg-black text-white flex justify-center gap-12 py-6">
         <select value={selectedValue} onChange={handleChange} className='px-4 py-4 rounded-md bg-black focus:outline-none cursor-pointer'>
-          <option value={"hex"}>HEX</option>
+          
+          {/* <option value={"hex"}>HEX</option>
           <option value={"rgb"}>RGB</option>
-          <option value={"hsl"}>HSL</option>
+          <option value={"hsl"}>HSL</option> */}
+          {
+            colorType.map((item)=>{
+              return <option value={item}>{item}</option>
+            })
+          }
         </select>
         <button className="generateColor border-none bg-white text-black px-4 py-2 rounded-lg" onClick={HandleRandomGenerate}>Generate Random Color</button>
       </div>
