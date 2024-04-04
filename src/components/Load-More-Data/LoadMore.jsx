df34bdf2d3c6f61a14ba8ce24ffeead52eb168e7
Punya Prashun <punya.prasun.cs24@iilmcet.ac.in>
2024-04-04 22:22:42 +0530

    LoadMore Completed with minor bug

--- src/components/Load-More-Data/LoadMore.jsx
+++ src/components/Load-More-Data/LoadMore.jsx
@@ -6,14 +6,15 @@ const LoadMore = () => {
     const [errMsg, setErrMsg] = useState(null);
     const [count, setCount] = useState(0);
 
-    async function fetchProducts() {
+    async function fetchProducts(ignore) {
         try {
             setLoading(true);
             const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`);
             const data = await response.json();
             if (data && data.products && data.products.length) {
-                setProducts(data.products);
-                setLoading(false);
+                    setProducts((prevData)=> [...prevData, ...data.products]);
+                    console.log(products)
+                    setLoading(false);
             }
         }
         catch (e) {
@@ -23,8 +24,9 @@ const LoadMore = () => {
     }
     useEffect(() => {
         fetchProducts();
-    }, []);
-    console.log(products);
+        
+    }, [count]);
+    // console.log(products);
     if (loading) {
         return <div>Loading Data...Please wait!</div>;
     }
@@ -32,21 +34,28 @@ const LoadMore = () => {
         return <div>Something went wrong!!</div>;
     }
     return (
-        <div className="product-container w-full grid grid-cols-4 max-lg:grid-cols-2 gap-4 px-6 py-8">
-            {
-                products && products.length
-                    ? products.map((Item) => (
-                        <div className="product px-4 py-4 border border-[rgba(0,0,0,0.2)] flex flex-col items-center justify-center gap-6" key={Item.id}>
-                            <div className="img-box h-56 w-56 ">
-                                <img src={Item.thumbnail} className='object-fill w-full h-full rounded-lg' alt={Item.title} />
+        <div className="main-container flex flex-col gap-5">
+            <div className="product-container w-full grid grid-cols-4 max-lg:grid-cols-2 gap-4 px-6 py-8">
+                {
+                    products && products.length
+                        ? products.map((Item) => (
+                            <div className="product px-4 py-4 border border-[rgba(0,0,0,0.2)] flex flex-col items-center justify-center gap-6" key={Item.id}>
+                                <div className="img-box h-56 w-56 ">
+                                    <img src={Item.thumbnail} className='object-fill w-full h-full rounded-lg' alt={Item.title} />
+                                </div>
+                                <p className="product-name text-center">
+                                    {Item.title}
+                                </p>
                             </div>
-                            <p className="product-name text-center">
-                                {Item.title}
-                            </p>
-                        </div>
-                    ))
-                    : null
-            }
+                        ))
+                        : null
+                }
+            </div>
+            <div className="load-more-box w-full flex items-center justify-center mb-10">
+                <button className="load-more px-4 py-2 rounded-lg text-white bg-black"
+                onClick={()=> setCount(count+1)}
+                >Load More</button>
+            </div>
         </div>
     )
 }
